65a300dec1975a045010f56d196c28cf27d44ade
Max Cooley <max@polygonal.place>
2025-03-24 09:50:55 -0700

    comment out swerve module logging because it's unnecessary

--- src/main/java/frc/robot/swerve/SwerveTelemetry.java
+++ src/main/java/frc/robot/swerve/SwerveTelemetry.java
@@ -130,26 +130,27 @@ public class SwerveTelemetry {
             SmartDashboard.putData("Module " + i, m_moduleMechanisms[i]);
         }
 
+        /* causing some problem with NT and we honestly don't really need it */
         /* Dashboard swerve drive */
-        SmartDashboard.putData("Swerve Drive", new Sendable() {
-            @Override
-            public void initSendable(SendableBuilder builder) {
-                builder.setSmartDashboardType("SwerveDrive");
-
-                builder.addDoubleProperty("Front Left Angle", () -> m_moduleDirections[0].getAngle(), null);
-                builder.addDoubleProperty("Front Left Velocity", () -> m_moduleSpeeds[0].getLength(), null);
-
-                builder.addDoubleProperty("Front Right Angle", () -> m_moduleDirections[0].getAngle(), null);
-                builder.addDoubleProperty("Front Right Velocity", () -> m_moduleSpeeds[0].getLength(), null);
-
-                builder.addDoubleProperty("Back Left Angle", () -> m_moduleDirections[0].getAngle(), null);
-                builder.addDoubleProperty("Back Left Velocity", () -> m_moduleSpeeds[0].getLength(), null);
-
-                builder.addDoubleProperty("Back Right Angle", () -> m_moduleDirections[0].getAngle(), null);
-                builder.addDoubleProperty("Back Right Velocity", () -> m_moduleSpeeds[0].getLength(), null);
-
-                builder.addDoubleProperty("Robot Angle", () -> state.Pose.getRotation().getDegrees(), null);
-            }
-        });
+//        SmartDashboard.putData("Swerve Drive", new Sendable() {
+//            @Override
+//            public void initSendable(SendableBuilder builder) {
+//                builder.setSmartDashboardType("SwerveDrive");
+//
+//                builder.addDoubleProperty("Front Left Angle", () -> m_moduleDirections[0].getAngle(), null);
+//                builder.addDoubleProperty("Front Left Velocity", () -> m_moduleSpeeds[0].getLength(), null);
+//
+//                builder.addDoubleProperty("Front Right Angle", () -> m_moduleDirections[0].getAngle(), null);
+//                builder.addDoubleProperty("Front Right Velocity", () -> m_moduleSpeeds[0].getLength(), null);
+//
+//                builder.addDoubleProperty("Back Left Angle", () -> m_moduleDirections[0].getAngle(), null);
+//                builder.addDoubleProperty("Back Left Velocity", () -> m_moduleSpeeds[0].getLength(), null);
+//
+//                builder.addDoubleProperty("Back Right Angle", () -> m_moduleDirections[0].getAngle(), null);
+//                builder.addDoubleProperty("Back Right Velocity", () -> m_moduleSpeeds[0].getLength(), null);
+//
+//                builder.addDoubleProperty("Robot Angle", () -> state.Pose.getRotation().getDegrees(), null);
+//            }
+//        });
     }
 }
